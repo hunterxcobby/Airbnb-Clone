bd84208f1666af874579fa424a875600d327a3ee
hunterxcobby <solomonsefah13@gmail.com>
2024-01-07 20:54:42 +0000

    add methods to our console to save and reload

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
+import json
 
 class UserManagement(cmd.Cmd):
     """simple CRUD command for managing users"""
@@ -9,7 +10,7 @@ class UserManagement(cmd.Cmd):
     def __init__(self):
         super().__init__()
         self.users = {} #dictionary to store users
-
+        
     def do_create(self, line):
         """create a new user Syntax: create <digit> <name>"""
         args = line.split()
@@ -19,9 +20,11 @@ class UserManagement(cmd.Cmd):
             print(f"User created - ID: {digit}, Name: {name}")
         else:
             print("Invalid input. Use: create <digit> <name>")
+        self.save_to_file()
 
     def do_read(self, line):
         """Read and Display all users"""
+        self.load_from()
         print("list of users:")
         for digit, name in self.users.items():
             print(f"ID: {digit} -> Name: {name}")
@@ -38,6 +41,7 @@ class UserManagement(cmd.Cmd):
                 print(f"No user found with - ID {digit}")
         else:
             print("Invalid input. Use: update <digit> <name>")
+        self.save_to_file()
 
     def do_destroy(self, line):
         """Delete a User by ID. Syntax: update <digit> <name> """
@@ -46,6 +50,21 @@ class UserManagement(cmd.Cmd):
             print(f"User deleted - ID: {line}")
         else:
             print(f"No user found with ID {line}")
+        self.save_to_file()
+
+    def save_to_file(self):
+        """save data to file"""
+        with open("user_data.json", "a") as json_file:
+            json.dump(self.users, json_file)
+
+    def load_from(self):
+        """loads the user data from the json file"""
+        with open("user_data.json", "r") as json_file:
+            self.users = json.load(json_file)
+
+            # print(self.users["4"])
+    
 
 if __name__ == '__main__':
+    UserManagement().load_from()
     UserManagement().cmdloop()
